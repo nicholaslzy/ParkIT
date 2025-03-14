ce7a3ae9bb5a99e602e648c02eaa42fc985412ee
quekdylan <quekdylan7@gmail.com>
2025-03-14 22:11:16 +0800

    temp

--- ParkIT/backend/src/main/java/website/parkit/ParkIT/Application.java
+++ ParkIT/backend/src/main/java/website/parkit/ParkIT/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ParkItApplication {
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkItApplication.class, args);
